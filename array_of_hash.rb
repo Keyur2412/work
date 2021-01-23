2f4f4a59fcf3589d353ccbb476b2da490e51cbce
kpatel <kpatel@C02D1631MD6M.local>
2021-01-23 01:05:59 -0800

    Add all my files

--- array_of_hash.rb
+++ array_of_hash.rb
@@ -1,8 +1,26 @@
 class ArrayOfHash
-  hash_arr = []
-  hash1 = {"name" => 'Keyur', "roll_no" => 123, "marks" => 90}
-  hash2 = {"name" => 'Krunal', "roll_no" => 124, "marks" => 99}
-  hash_arr.push(hash1)
-  hash_arr.push(hash2)
-  puts hash_arr
+  @@students = []
+
+  def add_student(name:, roll_no:, marks:)
+    student = {}
+    student[:name]    = name
+    student[:roll_no] = roll_no
+    student[:marks]   = marks
+    @@students.push(student)
+  end
+
+  def list_students
+    puts @@students
+  end
+
+  def result?( name)
+    result = @@students.any? {|student| (student[:name] == name && student[:marks] >= 35)}
+    puts result ? 'Pass' : 'Fail'
+  end
 end
+student_hash = ArrayOfHash.new
+student_hash.add_student(name: "test", roll_no: 1, marks: 34)
+student_hash.add_student(name: "test1", roll_no: 2, marks: 36)
+student_hash.add_student(name: "test2", roll_no: 3, marks: 90)
+student_hash.list_students
+student_hash.result?('test')
